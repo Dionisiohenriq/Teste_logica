cebf126c78ffe00e5b6229f527f86fb8f474cbd7
dionisiohenriq <henrique.contractor@am.jll.com>
2021-12-31 06:23:55 -0300

    Adição de DBContext e primeira migration, ainda não gerou as tabelas

---
+++ Teste_logica/MeuContexto.cs
@@ -0,0 +1,21 @@
+﻿using SQLite.CodeFirst;
+using System.Data.Entity;
+
+namespace Exercicio_1
+{
+    class MeuContexto: DbContext
+    {
+        public MeuContexto()
+        : base("C:\\Users\\USUARIO\\Documents\\meuDb.db") { }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            var sqliteConnectionInitializer = new SqliteCreateDatabaseIfNotExists<MeuContexto>(modelBuilder);
+            Database.SetInitializer(sqliteConnectionInitializer);
+        }
+
+        public DbSet<Compra> Compras { get; set; }
+        public  DbSet<Peca> Pecas { get; set; }
+
+    }
+}
---
+++ Teste_logica/Migrations/Configuration.cs
@@ -0,0 +1,23 @@
+﻿namespace Exercicio_1.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<Exercicio_1.MeuContexto>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(Exercicio_1.MeuContexto context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
