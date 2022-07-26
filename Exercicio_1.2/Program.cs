b44ff0623a34091a1feee9d6091f9a996af9a8af
dionisiohenriq <henrique.contractor@am.jll.com>
2022-07-26 07:03:44 -0300

    novos exercícios

--- Exercicio_1.2/Program.cs
+++ Exercicio_1.2/Program.cs
@@ -1,10 +1,12 @@
 ﻿
 var pi = Math.Round(Math.PI, 5);
 
-Console.WriteLine("Insira o raio de um círculo: \n"); 
-var raio = double.Parse(Console.ReadLine());
+Console.WriteLine("Insira o raio de um círculo: \n");
+var raio = double.TryParse(Console.ReadLine(), out var r);
 
-var area = Math.Round(pi * Math.Pow(raio, 2), 4);
-
-Console.WriteLine($"O raio é: {raio}\n" +
+if (raio)
+{
+    var area = Math.Round(pi * Math.Pow(r, 2), 4);
+    Console.WriteLine($"O raio é: {r}\n" +
                   $"A área é: {area}");
+}
